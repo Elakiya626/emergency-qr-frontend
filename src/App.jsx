d0ab2e7bb80a5890cf71678f362d6615e251ac07
Elakiya626 <elakiyamalaranand626@gmail.com>
2026-01-31 03:32:16 +0530

    Fix QR code component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import QRCode from "qrcode.react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>Emergency QR</h2>
+
+      <QRCode
+        value={window.location.origin + "/user/123"}
+        size={200}
+      />
+    </div>
+  );
+}
+
+export default App;
