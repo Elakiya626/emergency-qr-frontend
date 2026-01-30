574c77fa5de3785f101e50d8d1696cd1e35115c6
Elakiya626 <elakiyamalaranand626@gmail.com>
2026-01-31 03:37:32 +0530

    Fix QR localhost issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h2>Emergency QR</h2>
 
-      <QRCode
-        value={window.location.origin + "/user/123"}
-        size={200}
-      />
+     <QRCode
+  value={window.location.origin + "/user/123"}
+  size={200}
+/>
+
     </div>
   );
 }
